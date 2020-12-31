9b30935f2c6153197b85ab9b2ab7be603cfb838d
Yann Regis-Gianas <yann@nomadic-labs.com>
2020-12-31 09:40:20 +0100

    Deploy static instance.

---
+++ docs/js/learnocaml-config.js
@@ -0,0 +1,8 @@
+var learnocaml_config = {
+  enableTryocaml: false,
+  enablePlayground: false,
+  enableLessons: false,
+  enableExercises: true,
+  enableToplevel: true,
+  baseUrl: "https://ocaml-sf.github.io/learn-ocaml-public"
+}
